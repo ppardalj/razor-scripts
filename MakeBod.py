d06a042610ed8a0795c433f28fc9f790bfc7cdd7
Pedro Pardal <ppardalj@gmail.com>
2021-05-16 00:38:57 +0200

    feat(bods): add studded and female armor data

--- MakeBod.py
+++ MakeBod.py
@@ -186,7 +186,7 @@ items_info = {
         "category": 29
     },
     
-    # leather
+    # leather armor
     "leather gorget": {
         "gumpId": 23,
         "itemId": 5063,
@@ -216,6 +216,65 @@ items_info = {
         "gumpId": 58,
         "itemId": 5068,
         "category": 36
+    },
+    
+    # female armor
+    "leather shorts": {
+        "gumpId": 2,
+        "itemId": 7168,
+        "category": 50
+    },
+    "leather skirt": {
+        "gumpId": 9,
+        "itemId": 7176,
+        "category": 50
+    },
+    "leather bustier": {
+        "gumpId": 16,
+        "itemId": 7178,
+        "category": 50
+    },
+    "studded bustier": {
+        "gumpId": 23,
+        "itemId": 7180,
+        "category": 50
+    },
+    "female leather armor": {
+        "gumpId": 30,
+        "itemId": 7174,
+        "category": 50
+    },
+    "studded armor": {
+        "gumpId": 37,
+        "itemId": 7170,
+        "category": 50
+    },
+    
+    # studded armor
+    "studded gorget": {
+        "gumpId": 2,
+        "itemId": 5078,
+        "category": 43
+    },
+    "studded gloves": {
+        "gumpId": 9,
+        "itemId": 5068,
+        "category": 43
+    },
+    "studded sleeves": {
+        "gumpId": 16,
+        "itemId": 5068,
+        "category": 43
+    },
+    "studded leggings": {
+        "gumpId": 23,
+        "itemId": 5082,
+        "category": 43
+    },
+    "studded tunic": {
+        "gumpId": 30,
+        "itemId": 5083,
+        "category": 43
     }
 }
 
