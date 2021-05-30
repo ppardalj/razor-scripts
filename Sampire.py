ae1eaab05628116e6692694007f3a39705cfb1f6
Pedro Pardal <ppardalj@gmail.com>
2021-05-30 20:43:24 +0200

    feat(sampire): add actual cast

--- Sampire.py
+++ Sampire.py
@@ -2,15 +2,22 @@ from utils import log
 logger = log.Logger(Misc)
 
 while True:
-    if not Player.BuffsExist('Enemy Of One'):
-        logger.info('Should cast Enemy Of One')
-    if not Player.BuffsExist('Consecrate Weapon'):
-        logger.info('Should cast Consecrate Weapon')
-    if not Player.BuffsExist('Wraith Form'):
-        logger.info('Should cast Wraith Form')
-    if not Player.BuffsExist('Divine Fury'):
-        logger.info('Should cast Divine Fury')
     if not Player.BuffsExist('Curse Weapon'):
-        logger.info('Should cast Curse Weapon')
-
+        scroll = Items.FindByID(8803, -1, Player.Backpack.Serial)
+        Items.UseItem(scroll)
+        Misc.Pause(2000)
+        continue
+    if not Player.BuffsExist('Enemy Of One'):
+        Spells.CastChivalry('Enemy Of One')
+        Misc.Pause(2000)
+        continue
+    if not Player.BuffsExist('Divine Fury'):
+        Spells.CastChivalry('Divine Fury')
+        Misc.Pause(2000)
+        continue
+    if not Player.BuffsExist('Consecrate Weapon'):
+        Spells.CastChivalry('Consecrate Weapon')
+        Misc.Pause(2000)
+        continue
+    log.info('no cast needed')
     Misc.Pause(1000)
